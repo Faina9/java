671d2cd15364fe1b3d22bb9ccf3c149078ad1b0d
faina@vostro <f.a.ershova@gmail.com>
2021-12-08 21:47:47 +0300

    first java classes

---
+++ src/Bank.java
@@ -0,0 +1,19 @@
+
+
+public class Bank {
+
+    String name;
+    String license;
+
+    public Bank (String name, String license) {
+        this.name = name;
+        this.license = license;
+
+
+    }
+
+
+    public static void main (String[] args) {
+
+    }
+}
